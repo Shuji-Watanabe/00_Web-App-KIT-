ee038318ffc6564a57025c14bbdceb945541a522
Shuji-Watanabe <60733506+Shuji-Watanabe@users.noreply.github.com>
2021-10-02 09:01:11 +0900

    commit20211002-01

--- Streamlit_Sample05.py
+++ Streamlit_Sample05.py
@@ -10,10 +10,17 @@ st.title("""関数の極大点，極小点，変曲点""")
 FORM_INPUT = st.sidebar.text_input('極大点，極小点，変曲点を調べたい関数を入力')
 
 
+
 if not FORM_INPUT:
     st.write("数式をサイドバーに入力してください．")
 else :
     F0 = sympify(FORM_INPUT)
+    F1 = simplify(diff(F0))
+    F1_Expand = expand(simplify(diff(F0)))
+    F2 = simplify(diff(F1))
+    F3 = simplify(diff(F2))
+    X0 = sympify(solve(Eq(0,sympify(F1)),x))
+    X00 = sympify(solve(Eq(0,sympify(F2)),x))
     st.write('入力された関数は次の通りです．')
     st.latex(F0)
     """サイドバーの解答をみたい項目にチェックを入れてください．"""
@@ -22,9 +29,7 @@ else :
         ### **\[Step1：1次導関数および2次導関数の計算\]**\
         ',unsafe_allow_html=True)
         ### <span style="color:yellow"> \[Step1：1次導関数および2次導関数の計算\]</span>
-    F1 = simplify(diff(F0))
-    F1_Expand = expand(simplify(diff(F0)))
-    F2 = simplify(diff(F1))
+
     R01_check=st.sidebar.checkbox('1次導関数の計算結果')
     if (R01_check == 1) :
         st.write('▶︎ $f(x)\\,$の１次導関数は次の通りです．')
@@ -78,19 +83,19 @@ else :
         st.latex(PRINT_STR1)
 
     st.write('\
-        ### **\[Step3：極大点，極小点，変曲点の判定\]**',\
+        ### **\[Step3：極大点，極小点，変曲点の座標\]**',\
         unsafe_allow_html=True)  
         ### <span style="color:yellow"> \[Step3：極大点，極小点，変曲点の判定\]</span>'
-    MaxMimi_P=st.sidebar.checkbox('極大点，極小点の判定結果')
+    MaxMimi_P=st.sidebar.checkbox('極大点，極小点の座標')
     if (MaxMimi_P == 1) :
         j=0
         k=0
-        st.write('▶︎ 極大点，極小点')
+        st.write('▶︎ 極大点，極小点：極大点，極小点に関する説明は[こちらから](https://w3e.kanazawa-it.ac.jp/math/category/bibun/henkan-tex.cgi?target=/math/category/bibun/kyokuti.html)')
         for i in range(len(X0)):
             try:
                 aa=sympify(F2.subs(x,X0[i-1]))
             except:
-                st.write(' 判定不能')
+                st.write('　極大点または極小点であるかの判定不能')
             else:
                 STR_PRINT=''
                 STR_PRINT=STR_PRINT+"$\\left("+latex(X0[i-1])+",\ "+latex(sympify(F0.subs(x,X0[i-1])))
@@ -102,17 +107,16 @@ else :
                     k=k+1
                     st.write('$\quad$　極小点',latex(k),":",STR_PRINT)
         
-    Inflection_P=st.sidebar.checkbox('変曲点の判定結果')
+    Inflection_P=st.sidebar.checkbox('変曲点の座標')
     if (Inflection_P == 1) :
-        st.write('▶︎ 変曲点')
+        st.write('▶︎ 変曲点：変曲点に関する説明は[こちらから](https://w3e.kanazawa-it.ac.jp/math/category/bibun/henkan-tex.cgi?target=/math/category/bibun/inflection_point.html)')
         l=0
         for i in range(len(X00)):
             try:
-                F3 = simplify(diff(F2))
                 aa=sympify(F3.subs(x,X00[i-1]))
                 
             except:
-                st.write(' 判定不能')
+                st.write(' 変曲点であるかどうかの判定不能')
             else:
                 STR_PRINT=''
                 STR_PRINT=STR_PRINT+"$\\left("+latex(X00[i-1])+",\ "+latex(sympify(F0.subs(x,X00[i-1])))
