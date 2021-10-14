22dbc535f57bcf57e7bc98ae038d00e95566a908
Shuji-Watanabe <60733506+Shuji-Watanabe@users.noreply.github.com>
2021-10-14 21:27:01 +0900

    commit20211014_1

--- PHYS_Functions01.py
+++ PHYS_Functions01.py
@@ -6,26 +6,20 @@ from sympy import *
 ## 使い方
 ##   A = List_to_vec01(Val)
 ##   st.write(A) <- 横ベクトル化された式が出力される
- #################################################################
+#################################################################
 def List_to_vec01(Val):
     List_to_vec01_STR="\\displaystyle"
     List_to_vec01_STR=List_to_vec01_STR+" \\begin{pmatrix}"
     for i in range(len(Val)):
         if i < len(Val)-1 :
-            List_to_vec01_STR=List_to_vec01_STR + "\\displaystyle" +latex(Val[i])+"&"
+            List_to_vec01_STR=List_to_vec01_STR + "\\displaystyle " +latex(Val[i])+"&"
         else:
-            List_to_vec01_STR=List_to_vec01_STR + "\\displaystyle" +latex(Val[i])
+            List_to_vec01_STR=List_to_vec01_STR + "\\displaystyle " +latex(Val[i])
     List_to_vec01_STR=List_to_vec01_STR+"\\end{pmatrix}"
     return List_to_vec01_STR
 
 #### 時間に依存する物理量の平均変化率を計算する関数 #######################
-## 入力値：delta_t:時間の変化量
-## 入力値：delta_Q：Qの変化量，リスト型変数
-## 返り値：Average_Q：平均変化率，リスト型変数
-## 使い方
-##   A = Cal_Q_Average_ver01(dt,dQ)
-##   A <- 平均変化率がリスト型でAに格納される
- #################################################################
+#################################################################
 def Cal_Q_Average_ver01(delta_t,delta_Q):
     Q_A_Num00=len(delta_Q)
     Average_Q=[]
