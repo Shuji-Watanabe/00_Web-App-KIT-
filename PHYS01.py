d52ae23e7d8e362f9d33f27d97aa9ace13ab6b38
Shuji-Watanabe <60733506+Shuji-Watanabe@users.noreply.github.com>
2021-10-14 13:43:43 +0900

    commit20211014_first

--- PHYS01.py
+++ PHYS01.py
@@ -1,5 +1,25 @@
-def Add_1(*val):
-    Re_val=[]
-    for i in range(len(val)):
-        Re_val.append(val[i]+1)
-    return Re_val
+from sympy import *
+#import streamlit as st
+
+def List_to_vec01(Val):
+    List_to_vec01_STR="\\displaystyle"
+    List_to_vec01_STR=List_to_vec01_STR+" \\overrightarrow{v}_{\\rm Ave.} = "
+    List_to_vec01_STR=List_to_vec01_STR+" \\begin{pmatrix}"
+    for i in range(len(Val)):
+        if i < len(Val)-1 :
+            List_to_vec01_STR=List_to_vec01_STR + "\\displaystyle" +latex(Val[i])+"&"
+        else:
+            List_to_vec01_STR=List_to_vec01_STR + "\\displaystyle" +latex(Val[i])
+    List_to_vec01_STR=List_to_vec01_STR+"\\end{pmatrix}"
+    return List_to_vec01_STR
+
+def Add_1(delta_t,V_i,V_f):
+    Add_1_Num00=len(V_i)
+    Average_V=[]
+    for i in range(Add_1_Num00):
+        Average_V.append(
+              (sympify(V_f[i]) - sympify(V_i[i]))
+                /
+                delta_t
+         )
+    return Average_V
