3a0ea1ef1f2a119a29645c48de436a7a26dd77bd
Shuji-Watanabe <60733506+Shuji-Watanabe@users.noreply.github.com>
2021-10-14 17:03:23 +0900

    commit20211014_03

--- PHYS_A_Ave.py
+++ PHYS_A_Ave.py
@@ -2,16 +2,16 @@ import streamlit as st
 from sympy import *
 import PHYS_Functions01
 
-def Cal_Velocity_Ave(Cal_Ave_Num_Dim,Cal_Ave_Error_Str,Cal_Velocity_App_num):
+def Cal_Acceleration_Ave(Cal_Ave_Num_Dim,Cal_Ave_Error_Str,Cal_Acceleration_App_num):
     Error_Str=Cal_Ave_Error_Str
     Num_Dim=Cal_Ave_Num_Dim
-    App_num=Cal_Velocity_App_num
+    App_num=Cal_Acceleration_App_num
     E_01=0
-    st.write("### **Step1:時間の変化量**")
+    st.write("### Step1:時間の変化量")
 
-    Cal_V_select_str_dt="時間の変化量の値を，変化の前後から計算するか，直接与えるか選択してください.("+str(App_num)+")"
+    Cal_A_select_str_dt="時間の変化量の値を，変化の前後から計算するか，直接与えるか選択してください.("+str(App_num)+")"
     Select_cal_dt=st.selectbox(
-        Cal_V_select_str_dt,
+        Cal_A_select_str_dt,
         ('１）変化の前後', '２）直接'))
     Method_Cal_dt={'１）変化の前後':0,'２）直接':1}
 
@@ -45,15 +45,13 @@ def Cal_Velocity_Ave(Cal_Ave_Num_Dim,Cal_Ave_Error_Str,Cal_Velocity_App_num):
         if ECB : st.markdown(Error_Str)
 
 
-    st.write("### **Step2:位置の変化量**")
-
-    Cal_V_select_str_dr="位置の変化量の値を，変化の前後から計算するか，直接与えるか選択してください.("+str(App_num)+")"
-    Select_cal_dr=st.selectbox(
-                                Cal_V_select_str_dr,
-                                ('１）変化の前後', '２）直接')
-                                )
-    Method_Cal_dr={'１）変化の前後':0,'２）直接':1}
-    if Method_Cal_dr[Select_cal_dr]==0:
+    st.write("### Step2:速度の変化量")
+    Cal_A_select_str_dv="速度の変化量の値を，変化の前後から計算するか，直接与えるか選択してください.("+str(App_num)+")"
+    Select_cal_dv=st.selectbox(
+        Cal_A_select_str_dv,
+        ('１）変化の前後', '２）直接'))
+    Method_Cal_dv={'１）変化の前後':0,'２）直接':1}
+    if Method_Cal_dv[Select_cal_dv]==0:
         st.write("##### 変化する前・後の位置の各成分を入力してください．")
         E_02=0
         Nnn0=[1]
@@ -68,9 +66,9 @@ def Cal_Velocity_Ave(Cal_Ave_Num_Dim,Cal_Ave_Error_Str,Cal_Velocity_App_num):
             st.write("入力：")  
         for i in range(Num_Dim):
             with CB0_col_V[i+1]:
-                CB0_str_r_i.append( "変化前の位置の第"+str(int(i)+1)+"成分(App%2d)"%(App_num))
+                CB0_str_r_i.append( "変化前の速度の第"+str(int(i)+1)+"成分(App%2d)"%(App_num))
                 CB0_r_i.append(st.text_input(CB0_str_r_i[i]))
-                CB0_str_r_f.append( "変化後の位置の第"+str(int(i)+1)+"成分(App%2d)"%(App_num))
+                CB0_str_r_f.append( "変化後の速度の第"+str(int(i)+1)+"成分(App%2d)"%(App_num))
                 CB0_r_f.append(st.text_input(CB0_str_r_f[i]))
         try:
             Delta_r = []
@@ -82,7 +80,7 @@ def Cal_Velocity_Ave(Cal_Ave_Num_Dim,Cal_Ave_Error_Str,Cal_Velocity_App_num):
             E_02 = 1
 
     else:
-        st.write("##### 位置の変化量の各成分を入力してください．")
+        st.write("##### 速度の変化量の各成分を入力してください．")
         E_02=0
         Delta_r=[]
         CB0_str_r_d=[]
@@ -94,28 +92,26 @@ def Cal_Velocity_Ave(Cal_Ave_Num_Dim,Cal_Ave_Error_Str,Cal_Velocity_App_num):
             st.write("入力：")  
         for i in range(Num_Dim):
             with CB0_col_V[i+1]:
-                CB0_str_r_d.append( "位置の変化量の第"+str(int(i)+1)+"成分(App%2d)"%(App_num))
+                CB0_str_r_d.append( "速度の変化量の第"+str(int(i)+1)+"成分(App%2d)"%(App_num))
                 try:
                     Delta_r.append(sympify(st.text_input(CB0_str_r_d[i])))
                 except:
                     E_02 = 1
     
     if E_02==1 :
-        st.write("※ 位置の変化量の計算でエラーが出ています．")
+        st.write("※ 速度の変化量の計算でエラーが出ています．")
         ECB = st.checkbox("詳細表示(App%2d-%2d)"%(App_num,2))
         if ECB : st.markdown(Error_Str)
 
-
-    st.write("### **Step3:平均速度が出力されます．**")
+    st.write("### Step3:平均加速度が出力されます．")
 
     try:
         CB0_V_Average=PHYS_Functions01.Cal_Q_Average_ver01(Delta_t,Delta_r) 
     except:
-        st.write("※ 平均速度の計算でエラーが出ています．")
+        st.write("※ 平均加速度の計算でエラーが出ています．")
         ECB = st.checkbox("詳細表示(App%2d-%2d)"%(App_num,3))
         if ECB : st.markdown(Error_Str)
-
     else : 
-        Result_V_Ave1 = "\\displaystyle \\overrightarrow{v}_{\\rm Ave.} = "
+        Result_V_Ave1 = "\\displaystyle \\overrightarrow{a}_{\\rm Ave.} = "
         Result_V_Ave2 = PHYS_Functions01.List_to_vec01(CB0_V_Average)
         st.latex(Result_V_Ave1+Result_V_Ave2)
--- Sample_main01.py
+++ Sample_main01.py
@@ -20,6 +20,7 @@ st.sidebar.write(r"### %s次元中の運動を解析します"%(Dim[Select_Dim])
 ############# 終了
 #
 ############# コンテンツの指定：開始
+App_num=0
 CB=[]
 CB.append(st.sidebar.checkbox('平均速度'))
 CB.append(st.sidebar.checkbox('平均加速度'))
@@ -29,27 +30,20 @@ if sum(CB) == 0:
 #
 ############# 平均速度の計算コンテンツ：開始
 if CB[0]:
-    import PHYS_Functions01   
+    import PHYS_Functions01
     import PHYS_V_Ave
-    PHYS_V_Ave.Cal_Ave(Num_Dim,Error_Str)
+    App_num=App_num+1
+    st.write(r"## **○ 平均速度の計算アプリ(%s)**"%(App_num))
+    PHYS_V_Ave.Cal_Velocity_Ave(Num_Dim,Error_Str,App_num)
+    st.write(r"### --------------------------------------------------------------------アプリ(%s) End"%(App_num))
 ############# コンテンツ終了
 #
-############# 平均速度の計算コンテンツ：開始
+############# 平均加速度の計算コンテンツ：開始
 if CB[1]:
-    import PHYS_Functions01 
-    CB01_col= st.columns(Num_Dim)
-    CB01_PRAM=[]
-    str01=[]
-    for i in range(Num_Dim):
-        with CB01_col[i]:
-            str01.append( "変化前の加速度：第"+str(i)+"成分")
-            CB01_PRAM.append(st.text_input(str01[i]))
-            #st.write(str01[i])
-    try:
-        XX=PHYS_Functions01.Cal_V_Average_ver01(CB01_PRAM)    
-    except:
-        st.write("必要な変数を入力してください．")
-    else : 
-        for i in range(len(XX)):
-            st.write(r"変化前の速度：第%s成分=$%s$"%((i,XX[i])))
+    App_num=App_num+1
+    st.write(r"## **○ 平均加速度の計算アプリ(%s)**"%(App_num))
+    import PHYS_Functions01   
+    import PHYS_A_Ave
+    PHYS_A_Ave.Cal_Acceleration_Ave(Num_Dim,Error_Str,App_num)
+    st.write(r"### --------------------------------------------------------------------アプリ(%s) End"%(App_num))
 ############# コンテンツ終了
