53aa153342c7a457b0ef670fb0d3bdf5b4f60b26
Shuji-Watanabe <60733506+Shuji-Watanabe@users.noreply.github.com>
2021-10-15 12:40:17 +0900

    commit20211015_first

--- Sample_main01.py
+++ Sample_main01.py
@@ -1,50 +1,55 @@
 import streamlit as st
 from sympy import *
 
-
-
 ############# 典型的なエラー：開始
 Error_Str = "　$\qquad$Check.1　全ての変数に，値または式が入力されていますか？\n\n "
 Error_Str = Error_Str + "$\qquad$Check.2　半角で入力されていますか？\n\n "
 Error_Str = Error_Str + "$\qquad$Check.3　式の書き方に問題ありませんか？ \n\n"
 Error_Str = Error_Str + "$\qquad$※　\"$\Delta x$\"　は　Symbol(\"\Delta x\")　と書く．"
 ############# 終了
-#
 ############# 空間次元の指定：開始
-Select_Dim=st.sidebar.selectbox(
+st.sidebar.write(r"### ＜空間次元の指定＞")
+radio_Dim=st.sidebar.radio(
         '運動の解析を行う次元をしてください',
         ('１次元', '２次元', '３次元'))
-
+st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
 Dim={'１次元':1,'２次元':2,'３次元':3}
-Num_Dim=Dim[Select_Dim]
-st.sidebar.write(r"### %s次元中の運動を解析します"%(Dim[Select_Dim]))
+Dim_Num=Dim[radio_Dim]
 ############# 終了
-#
 ############# コンテンツの指定：開始
+st.sidebar.write(r"### ＜自動計算コンテンツの指定＞")
 App_num=0
 CB=[]
 CB.append(st.sidebar.checkbox('平均速度'))
 CB.append(st.sidebar.checkbox('平均加速度'))
+CB.append(st.sidebar.checkbox('位置，速度，加速度'))
 if sum(CB) == 0:
     st.write("コンテンツを選んでください．")
 ############# 終了
 #
 ############# 平均速度の計算コンテンツ：開始
 if CB[0]:
-    import PHYS_Functions01
-    import PHYS_V_Ave
+    import PHYS_V_Ave_ver2
     App_num=App_num+1
-    st.write(r"## **○ 平均速度の計算アプリ(%s)**"%(App_num))
-    PHYS_V_Ave.Cal_Velocity_Ave(Num_Dim,Error_Str,App_num)
+    st.write(r"# **平均速度(%s)**"%(App_num))
+    PHYS_V_Ave_ver2.Cal_Velocity_Ave(Dim_Num,Error_Str,App_num)
     st.write(r"### --------------------------------------------------------------------アプリ(%s) End"%(App_num))
 ############# コンテンツ終了
 #
 ############# 平均加速度の計算コンテンツ：開始
 if CB[1]:
     App_num=App_num+1
-    st.write(r"## **○ 平均加速度の計算アプリ(%s)**"%(App_num))
-    import PHYS_Functions01   
-    import PHYS_A_Ave
-    PHYS_A_Ave.Cal_Acceleration_Ave(Num_Dim,Error_Str,App_num)
+    st.write(r"# **平均加速度(%s)**"%(App_num))
+    import PHYS_A_Ave_ver2
+    PHYS_A_Ave_ver2.Cal_Acceleration_Ave(Dim_Num,Error_Str,App_num)
     st.write(r"### --------------------------------------------------------------------アプリ(%s) End"%(App_num))
+############# コンテンツ終了
+#
+############# 位置，速度，加速度の計算コンテンツ：開始
+# if CB[2]:
+#     App_num=App_num+1
+#     st.write(r"# **位置，速度，加速度の関係(%s)**"%(App_num))
+#     import PHYS_A_Ave_ver2
+#     PHYS_A_Ave_ver2.Cal_Acceleration_Ave(Dim_Num,Error_Str,App_num)
+#     st.write(r"### --------------------------------------------------------------------アプリ(%s) End"%(App_num))
 ############# コンテンツ終了
