73bfca641fa292b230dac76212f215a51a6d6b67
Shuji-Watanabe <60733506+Shuji-Watanabe@users.noreply.github.com>
2022-02-05 00:23:49 +0900

    20220204-37

--- 03_TEST/Main_Exercises_Wave_03.py
+++ 03_TEST/Main_Exercises_Wave_03.py
@@ -46,10 +46,10 @@ st.markdown("""
     小物体が静止する位置を原点$\\rm O$ とする．小物体が運動を開始した時刻を $t=0 \\rm\ s$ とするとき，
     時刻 $t \\rm\ [s]$ における小物体の運動の様子を考察する．
 """)
-image = Image.open('Fig01.svg')
-#st.image('単振動01.png',use_column_width=True,caption='ある時刻における小物体の様子')
-st.image(image,caption='ある時刻における小物体の様子')
 
+#st.image('単振動01.png',use_column_width=True,caption='ある時刻における小物体の様子')
+image = Image.open('fig01.jpg')
+st.image(image)
 
 
 tmp_CB01=st.sidebar.checkbox("ばね振り子の質量，ばね定数，初期条件を変更")
