3762619e1263d63644cdd32920abe3c8a41a8f34
Shuji-Watanabe <60733506+Shuji-Watanabe@users.noreply.github.com>
2022-09-15 12:43:42 +0900

    20220914_ve03

--- 100_Demo/00_回帰分析.py
+++ 100_Demo/00_回帰分析.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
+#from sklearn.linear_model import LinearRegression
 
 ### Main program ###
 st.title("### 単回帰分析")
