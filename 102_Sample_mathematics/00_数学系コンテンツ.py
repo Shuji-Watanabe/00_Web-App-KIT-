0840a79da63d03a5ea68f9c70ef0228f907f5a70
Shuji-Watanabe <60733506+Shuji-Watanabe@users.noreply.github.com>
2022-09-19 02:33:49 +0900

    20220919-ver02

--- 102_Sample_mathematics/00_数学系コンテンツ.py
+++ 102_Sample_mathematics/00_数学系コンテンツ.py
@@ -6,4 +6,8 @@ import japanize_matplotlib
 from matplotlib import patches
 
 ### Main program ###
-# st.title("数学系コンテンツ")
+st.title("数学系コンテンツ")
+"""
+    　このコンテンツは，数学的な考え方や数式の理解を助けることを目的としたコンテンツです．
+    各種課題に取り組む際の参考資料としてご利用ください．
+"""
