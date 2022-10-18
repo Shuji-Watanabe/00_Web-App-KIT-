b29f76fa97481dfd1133c28da78b959c285a699c
Shuji-Watanabe <60733506+Shuji-Watanabe@users.noreply.github.com>
2022-10-18 18:49:53 +0900

    20221018-ver06-test

---
+++ 07_Data_science/00_📊_データサイエンス基礎I.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import japanize_matplotlib
+from matplotlib import patches
+
+
+#st.set_page_config(layout="wide")
+### Main program ###
+st.title("データサイエンス基礎Ⅰ")
+"""
+    　データサイエンス基礎Iの学習支援アプリです．参考にしてください．
+"""
