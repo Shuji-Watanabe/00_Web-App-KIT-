b2f669172db7a92a3a0b8700554f974cad1e1a86
Shuji-Watanabe <60733506+Shuji-Watanabe@users.noreply.github.com>
2022-10-18 18:55:40 +0900

    20221018-ver06-test

--- 03_TEST/00_📊_データサイエンス基礎I.py
+++ 03_TEST/00_📊_データサイエンス基礎I.py
@@ -1,9 +1,5 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import japanize_matplotlib
-from matplotlib import patches
+
 
 
 #st.set_page_config(layout="wide")
