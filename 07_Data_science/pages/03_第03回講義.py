36b00361c41e494de1c4cdb665bffe745412ba67
Shuji-Watanabe <60733506+Shuji-Watanabe@users.noreply.github.com>
2022-10-18 19:51:27 +0900

    20221018-ver08

--- 07_Data_science/pages/03_第03回講義.py
+++ 07_Data_science/pages/03_第03回講義.py
@@ -132,8 +132,8 @@ with Lec03_contents_tab[contents_num]:
     section_title02="##### %s-%s　散布図行列（ペアプロット図）"%(contents_num+1,section_num)
     st.markdown(section_title02)
     # fig=plt.figure()
-    fig=sns.pairplot(inputdata_lec3_tub2)
-    st.pyplot(fig)
+    # fig=sns.pairplot(inputdata_lec3_tub2,corner=True,kind='reg')
+    # st.pyplot(fig)
     st.write(" ")
 
     #-----  section 3 : Dispay a Correlation coefficient matrix --------------------------
