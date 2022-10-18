26a31e3b690e5a48387d85b16eab729a794b6c38
Shuji-Watanabe <60733506+Shuji-Watanabe@users.noreply.github.com>
2022-10-18 19:46:03 +0900

    20221018-ver07

--- 07_Data_science/pages/03_第03回講義.py
+++ 07_Data_science/pages/03_第03回講義.py
@@ -131,7 +131,8 @@ with Lec03_contents_tab[contents_num]:
     section_num += 1 
     section_title02="##### %s-%s　散布図行列（ペアプロット図）"%(contents_num+1,section_num)
     st.markdown(section_title02)
-    fig=sns.pairplot(inputdata_lec3_tub2,corner=True)
+    
+    fig=sns.pairplot(inputdata_lec3_tub2,corner=True,kind='reg')
     st.pyplot(fig)
     st.write(" ")
 
