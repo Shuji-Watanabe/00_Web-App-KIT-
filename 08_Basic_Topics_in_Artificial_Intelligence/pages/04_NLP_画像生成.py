b851c83c46ee9da07e99ba1633e42aa2a77aa85e
Shuji-Watanabe <60733506+Shuji-Watanabe@users.noreply.github.com>
2022-11-29 18:43:51 +0900

    20221129-ver01

--- 08_Basic_Topics_in_Artificial_Intelligence/pages/04_NLP_画像生成.py
+++ 08_Basic_Topics_in_Artificial_Intelligence/pages/04_NLP_画像生成.py
@@ -6,15 +6,16 @@ from japanese_stable_diffusion import JapaneseStableDiffusionPipeline
 
 model_id = "rinna/japanese-stable-diffusion"
 device = "cuda"
+YOUR_TOKEN = "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"
 # Use the K-LMS scheduler here instead
 scheduler = LMSDiscreteScheduler(beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear", num_train_timesteps=1000)
 pipe = JapaneseStableDiffusionPipeline.from_pretrained(model_id, scheduler=scheduler, use_auth_token=True)
-# pipe = pipe.to(device)
+pipe = pipe.to(device)
 
 prompt = "猫の肖像画 油絵"
 
-# with autocast("cuda"):
-#     image = pipe(prompt, guidance_scale=7.5)["sample"][0]  
-image = pipe(prompt, guidance_scale=7.5)["sample"][0]
+with autocast("cuda"):
+    image = pipe(prompt, guidance_scale=7.5)["sample"][0]  
+# image = pipe(prompt, guidance_scale=7.5)["sample"][0]
 st.image(image)
 image.save("output.png")
