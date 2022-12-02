1571504434e1e40f175cdd09c7d1c9cf2ccf680e
Shuji-Watanabe <60733506+Shuji-Watanabe@users.noreply.github.com>
2022-12-03 07:12:46 +0900

    20221203-ver1

--- 08_Basic_Topics_in_Artificial_Intelligence/pages/04_NLP_画像生成.py
+++ 08_Basic_Topics_in_Artificial_Intelligence/pages/04_NLP_画像生成.py
@@ -1,20 +1,20 @@
-import streamlit as st
-import torch
-from torch import autocast
-from diffusers import LMSDiscreteScheduler
-from japanese_stable_diffusion import JapaneseStableDiffusionPipeline
+# import streamlit as st
+# import torch
+# from torch import autocast
+# from diffusers import LMSDiscreteScheduler
+# from japanese_stable_diffusion import JapaneseStableDiffusionPipeline
 
-YOUR_TOKEN = "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"
-model_id = "rinna/japanese-stable-diffusion"
-device = "cuda"
-# Use the K-LMS scheduler here instead
-scheduler = LMSDiscreteScheduler(beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear", num_train_timesteps=1000)
-pipe = JapaneseStableDiffusionPipeline.from_pretrained(model_id, scheduler=scheduler, use_auth_token=True)
-pipe = pipe.to(device)
+# YOUR_TOKEN = "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"
+# model_id = "rinna/japanese-stable-diffusion"
+# device = "cuda"
+# # Use the K-LMS scheduler here instead
+# scheduler = LMSDiscreteScheduler(beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear", num_train_timesteps=1000)
+# pipe = JapaneseStableDiffusionPipeline.from_pretrained(model_id, scheduler=scheduler, use_auth_token=True)
+# pipe = pipe.to(device)
 
 
-prompt = "油絵のたこ焼き"
-with autocast("cuda"):
-    image = pipe(prompt, guidance_scale=7.5).images[0]
-st.image(image)
-image.save("output.png")
+# prompt = "油絵のたこ焼き"
+# with autocast("cuda"):
+#     image = pipe(prompt, guidance_scale=7.5).images[0]
+# st.image(image)
+# image.save("output.png")
